9ca35aa2c93f589e9be482171344d0065531d057
Maksym Hyrya <maksimgira22@gmail.com>
2022-09-27 15:21:43 +0300

    fix all files

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,35 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 
+
 import $ from "jquery";
 import "slick-carousel";
 
+$(function () {
+	$('.auction__inner').slick({
+		arrows: true,
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		rightMode: true,
+		variableWidth: true
+	});
+});
+
+$('.collection__inner').slick({
+	arrows: true,
+	dots: false,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 2,
+	rightMode: true,
+	variableWidth: true
+});
+
+
 
 //--------------- BURGER-MENU ---------------//
-
 function burgerMenu(selector) {
 	let menu = $(selector);
 	let button = menu.find('.menu-header__button', '.menu-header__lines');
@@ -33,92 +56,58 @@ function burgerMenu(selector) {
 		}
 	}
 }
-
 burgerMenu('.menu-header');
+
 
 const mainElement = document.documentElement;
 const element = mainElement.clientWidth;
 const innerExplore = document.querySelector(".explore__inner");
-const secondBlock = document.querySelector(".explore__second-block");
-const mediaExplore = document.querySelector(".media-explore");
 
-
-if (element < 1165) {
-	const over = innerExplore.innerHTML(`
-<div class="explore__media media-explore">
-						<div class="media-explore__info">
-							<h4 class="media-explore__title">32k+</h4>
-							<p class="media-explore__text">Artworks</p>
-						</div>
-						<div class="media-explore__info">
-							<h4 class="media-explore__title">20k+</h4>
-							<p class="media-explore__text">Auctions</p>
-						</div>
-						<div class="media-explore__info">
-							<h4 class="media-explore__title">12k+</h4>
-							<p class="media-explore__text">Creators</p>
-						</div>
-					</div>
-				</div>
-				<div class="explore__second-block">
-					<div class="explore__current-bid current-bid">
-						<div class="current-bid__box">
-							<p class="current-bid__subtitle">Current Bid</p>
-							<h5 class="current-bid__title">0.99ETH</h5>
-						</div>
-						<div class="current-bid__box">
-							<p class="current-bid__subtitle">Ends in</p>
-							<h5 class="current-bid__title">25 hrs</h5>
-						</div>
-					</div>
-					<div class="explore__start-bid start-bid">
-						<div class="start-bid__box">
-							<h6 class="start-bid__title">Artist</h6>
-							<p class="start-bid__text">@wzard</p>
-						</div>
-						<button class="start-bid__btn">Start Bid</button>
-					</div>
-				</div>
-`);
-
-}
-
-
-
-
-//--------------CONTENT MORE--------------//
-/*
-
-
-
-const btn = document.querySelector("#btn");
-const more = document.querySelector(".content");
-
-btn.addEventListener("click", () => {
-	more.style.display = "flex"
-});
-
-
-
-btn.addEventListener("click", btnClick);
-function btnClick() {
-	console.log(content.classList);
-
-	if (content.classList.contains("menu-prices__more")) {
-		btn.textContent = "Скрыть элемент";
-	} else {
-		btn.textContent = "Показать элемент";
+function work() {
+	if (element < 1065) {
+		const over = innerExplore.innerHTML = `
+		<div class="explore__first-block">
+		<h1 class="explore__title">Explore, Buy and Sell the <span>Best NFTs!</span></h1>
+		<div class="explore__buttons">
+			<button class="explore__btn">Explore</button>
+			<button class="explore__btn">Create</button>
+		</div>
+	</div>
+			class="explore__img-scroll"></a>
+	<div class="explore__second-block">
+		<div class="explore__current-bid current-bid">
+			<div class="current-bid__box">
+				<p class="current-bid__subtitle">Current Bid</p>
+				<h5 class="current-bid__title">0.99ETH</h5>
+			</div>
+			<div class="current-bid__box">
+				<p class="current-bid__subtitle">Ends in</p>
+				<h5 class="current-bid__title">25 hrs</h5>
+			</div>
+		</div>
+		<div class="explore__start-bid start-bid">
+			<div class="start-bid__box">
+				<h6 class="start-bid__title">Artist</h6>
+				<p class="start-bid__text">@wzard</p>
+			</div>
+			<button class="start-bid__btn">Start Bid</button>
+		</div>
+	</div>
+	<div class="explore__media media-explore">
+	<div class="media-explore__info">
+		<h4 class="media-explore__title">32k+</h4>
+		<p class="media-explore__text">Artworks</p>
+	</div>
+	<div class="media-explore__info">
+		<h4 class="media-explore__title">20k+</h4>
+		<p class="media-explore__text">Auctions</p>
+	</div>
+	<div class="media-explore__info">
+		<h4 class="media-explore__title">12k+</h4>
+		<p class="media-explore__text">Creators</p>
+	</div>
+	</div>
+	`;
 	}
-
-	content.classList.toggle("menu-prices__more");
-};
-*/
-
-//---------- SLIDERS ----------//
-
-/*
-$('.menu').on('click', function () {
-	$('.header__list').toggleClass('header__list--active')
-});
-*/
-/*	document.getElementById("a1").innerHTML = "All";*/
+}
+work();
